fe1c328c9694644ee0f21363385c414f79c500ca
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-01-26 00:57:31 -0800

    Update task

--- src/index.js
+++ src/index.js
@@ -5,3 +5,18 @@ const allTask = document.querySelector('.displayed-element');
 const nouveauTask = document.querySelector('#task-description');
 const addTaskForm = document.querySelector('#new-task');
 const resete = document.querySelector('.refresh');
+
+const clearAll = () => {
+    Taskarray = [];
+  };
+  
+  const addListToLocalStorage = () => {
+    localStorage.setItem('myTodoTasks', JSON.stringify(Taskarray));
+  };
+  
+  const getListFromLocalStorage = () => {
+    if (localStorage.getItem('myTodoTasks')) {
+      Taskarray = Array.from(JSON.parse(localStorage.getItem('myTodoTasks')));
+    }
+    return Taskarray;
+  };
