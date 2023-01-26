60ff29e82e1f20b631f5bb59001aa8edd76a802b
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-01-26 00:43:04 -0800

    Add event

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+
+let Taskarray = [];
+const allTask = document.querySelector('.displayed-element');
+const nouveauTask = document.querySelector('#task-description');
+const addTaskForm = document.querySelector('#new-task');
+const resete = document.querySelector('.refresh');
